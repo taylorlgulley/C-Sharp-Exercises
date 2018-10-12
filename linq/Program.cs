8279d397bcc6d59632dd9781c5d02c325de3d632
Taylor Gulley <taylorlgulley@yahoo.com>
2018-10-12 15:53:47 -0500

    fixed git ignore and linq

---
+++ linq/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    public class SameBank
+    {
+        public string Name { get; set; }
+        public int NumberOfCustomers { get; set; }
+    }
+    // Define a bank
+    public class Bank
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+    }
+
+    // Define a customer
+    public class Customer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+    }
+    public class ReportItem
+    {
+        public string CustomerName { get; set; }
+        public string BankName { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
+
+            IEnumerable<string> LFruits = from fruit in fruits
+                where fruit.StartsWith("L")
+                select fruit;
+
+            // foreach (var fruit in LFruits)
+            // {
+            //     Console.WriteLine(fruit);
+            // }
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers1 = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers1.Where(n => n % 4 == 0 || n % 6 == 0);
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                "Francisco", "Tre"
+            };
+
+            // I did it both ways but I had to change the List to IEnumerable
+            // IEnumerable<string> descend = names.OrderByDescending(name => name);
+            IEnumerable<string> descend = from name in names
+                orderby name descending
+                select name;
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> numbers2 = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+            IEnumerable<int> ascend = from num in numbers2
+                orderby num
+                select num;
+
+            // Output how many numbers are in this list
+            List<int> numbers3 = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+            int numbers3Count = numbers3.Count();
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+            double totalMade = purchases.Sum();
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+            double highPrice = prices.Max();
+
+            /*
+                Store each number in the following List until a perfect square
+                is detected.
+
+                Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            */
+            List<int> wheresSquaredo = new List<int>()
+            {
+                66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            };
+            IEnumerable<int> numbersUntilSquare = wheresSquaredo.TakeWhile(num => Math.Sqrt(num) % 1 != 0);
+
+            List<Customer> customers = new List<Customer>() {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+            /*
+                Given the same customer set, display how many millionaires per bank.
+                Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+                Example Output:
+                WF 2
+                BOA 1
+                FTB 1
+                CITI 1
+            */
+            List<SameBank> sameBank = (from customer in customers
+                where customer.Balance >= 1000000
+                group customer by customer.Bank into customerBankGroup
+                select new SameBank {
+                    Name = customerBankGroup.Key,
+                    NumberOfCustomers = customerBankGroup.Count()
+                }).ToList();
+            foreach (var customer in sameBank)
+            {
+                Console.WriteLine($"{customer.Name} {customer.NumberOfCustomers}");
+            }
+
+            /*
+                TASK:
+                As in the previous exercise, you're going to output the millionaires,
+                but you will also display the full name of the bank. You also need
+                to sort the millionaires' names, ascending by their LAST name.
+
+                Example output:
+                    Tina Fey at Citibank
+                    Joe Landy at Wells Fargo
+                    Sarah Ng at First Tennessee
+                    Les Paul at Wells Fargo
+                    Peg Vale at Bank of America
+            */
+            // Create some banks and store in a List
+            List<Bank> banks = new List<Bank>() {
+                new Bank(){ Name="First Tennessee", Symbol="FTB"},
+                new Bank(){ Name="Wells Fargo", Symbol="WF"},
+                new Bank(){ Name="Bank of America", Symbol="BOA"},
+                new Bank(){ Name="Citibank", Symbol="CITI"},
+            };
+
+            /*
+                You will need to use the `Where()`
+                and `Select()` methods to generate
+                instances of the following class.
+
+                public class ReportItem
+                {
+                    public string CustomerName { get; set; }
+                    public string BankName { get; set; }
+                }
+            */
+            // List<ReportItem> millionaireReport = customers.Where(c => c.Balance >= 1000000).Select();
+            List<ReportItem> millionaireReport = (from customer in customers
+                where customer.Balance >= 1000000
+                orderby customer.Name.Split(' ')[1]
+                join bank in banks on customer.Bank equals bank.Symbol
+                select new ReportItem {
+                    CustomerName = customer.Name,
+                    BankName = bank.Name
+                }).ToList();
+
+            foreach (var item in millionaireReport)
+            {
+                Console.WriteLine($"{item.CustomerName} at {item.BankName}");
+            }
+        }
+    }
+}
